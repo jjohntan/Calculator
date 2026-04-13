e2f2226d74f9ac55e96e3e19203c09b108b729e2
tanjekjohn@gmail.com <tanjekjohn@gmail.com>
2026-04-13 18:41:44 +0800

    add: handle no argument found

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String [] args)
+    {
+        if (args.length <= 0)
+        {
+            System.out.println("No argument found");
+            System.exit(1);
+        }
+        System.out.println(">>>");
+    }
+}
