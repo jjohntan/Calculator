fceabf4547e4627f349357c3057f122fedc71d80
tanjekjohn@gmail.com <tanjekjohn@gmail.com>
2026-04-13 23:17:11 +0800

    add: read user input for second number

--- Main.java
+++ Main.java
@@ -3,10 +3,14 @@ import java.util.Scanner;// Import the Scanner class
 public class Main {
     public static void main(String [] args)
     {
+        Double nbr1, nbr2, res;
         Scanner inputObj = new Scanner(System.in);// create scanner object
 
-        Double nbr1 = inputObj.nextDouble();// Read user input
-        System.out.print(nbr1);
+        System.out.print(">>> ");
+        nbr1 = inputObj.nextDouble();// Read user input
+        System.out.println(nbr1);
+        nbr2 = inputObj.nextDouble();
+        System.out.println(nbr2);
         inputObj.close();
     }
 }
