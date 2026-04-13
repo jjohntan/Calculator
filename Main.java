eabd11a78e40856fc179ccf8a1ca2591247335be
tanjekjohn@gmail.com <tanjekjohn@gmail.com>
2026-04-13 23:33:31 +0800

    add: addition between numbers

--- Main.java
+++ Main.java
@@ -8,14 +8,26 @@ public class Main {
         Scanner inputObj = new Scanner(System.in);// create scanner object
 
         System.out.print(">>> ");
-        nbr1 = inputObj.nextDouble();// Read user input
+        nbr1 = inputObj.nextDouble();// read first number
         System.out.println(nbr1);
 
-        opt = inputObj.next().charAt(0);
+        opt = inputObj.next().charAt(0);// read operator
         System.out.println(opt);
 
-        nbr2 = inputObj.nextDouble();
+        nbr2 = inputObj.nextDouble();// read second number
         System.out.println(nbr2);
+
+        switch (opt) {
+            // performs addition between numbers
+            case '+':
+                res = nbr1 + nbr2;
+                System.out.println(res);
+                break;
+        
+            default:
+                System.out.println("Invalid Operator!");
+                break;
+        }
         inputObj.close();
     }
 }
